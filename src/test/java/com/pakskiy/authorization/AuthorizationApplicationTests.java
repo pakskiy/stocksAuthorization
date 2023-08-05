3329e175fea95766fc2b6a6882c63d4b6f3701c4
Oleg <oleg@shazzle.com>
2023-08-06 03:00:06 +0500

    Start cover by tests

--- src/test/java/com/pakskiy/authorization/AuthorizationApplicationTests.java
+++ src/test/java/com/pakskiy/authorization/AuthorizationApplicationTests.java
@@ -1,5 +1,6 @@
 package com.pakskiy.authorization;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -13,17 +14,25 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 @ActiveProfiles("test")
 @Testcontainers
+@RequiredArgsConstructor
 class AuthorizationApplicationTests {
+//    final AuthControllerTest authControllerTest;
+
     private static final DockerImageName dockerImageName = DockerImageName.parse("postgres:12.15");
     @Container
-    private final static PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer(dockerImageName)
+    private final static PostgreSQLContainer postgresqlContainer = (PostgreSQLContainer) new PostgreSQLContainer(dockerImageName)
             .withDatabaseName("stockAuth")
             .withUsername("postgres")
-            .withPassword("123456");
+            .withPassword("123456").withReuse(true);
 
     @Test
-    void contextLoads() {
+    void checkContainerIsRunning() {
         assertTrue(postgresqlContainer.isRunning());
     }
+
+//    @Test
+//    void checkRegisterMethod(){
+//        authControllerTest.register();
+//    }
 
 }
