766e0936dae22d0a9f98f5a9ffb461cc417686ef
Oleg <oleg@shazzle.com>
2023-08-13 22:58:21 +0500

    Added login method to AuthService

--- src/main/java/com/pakskiy/authorization/service/AuthService.java
+++ src/main/java/com/pakskiy/authorization/service/AuthService.java
@@ -1,10 +1,15 @@
 package com.pakskiy.authorization.service;
 
+import com.pakskiy.authorization.config.JwtService;
+import com.pakskiy.authorization.dto.LoginRequestDto;
+import com.pakskiy.authorization.dto.LoginResponseDto;
 import com.pakskiy.authorization.dto.RegisterRequestDto;
 import com.pakskiy.authorization.model.User;
 import com.pakskiy.authorization.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +19,8 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     public void register(RegisterRequestDto request) {
         var user = User.builder()
@@ -22,5 +29,19 @@ public class AuthService {
                 .password(passwordEncoder.encode(request.getPassword()))
                 .build();
         userRepository.save(user);
+    }
+
+    public LoginResponseDto login(LoginRequestDto request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(), request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+
+        return LoginResponseDto.builder()
+                .token(jwtToken)
+                .build();
     }
 }
